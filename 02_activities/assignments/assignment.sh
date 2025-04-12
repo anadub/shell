c2771051c251b3ed4aab2c24f5ec0e9e1b22414a
anadub <anadub@gmail.com>
2025-04-12 10:52:03 -0400

    Finish Q5

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -23,7 +23,7 @@ unzip -q rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-unzip  -oq  rawdata.zip
+#unzip  -oq  rawdata.zip
 cd data
 mkdir raw
 cd  ..
